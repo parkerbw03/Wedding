c981f62b02c2b3019eb146ad04d925165ab011bd
Ben Parker <parkerbw03@gmail.com>
2016-02-18 20:02:28 -0600

    added target links

--- registry.php
+++ registry.php
@@ -1,9 +1,12 @@
 <?php include("nav.php"); ?>
+
+<!--
 <script>
 function goBack() {
     window.history.back();
 }
 </script>
+
 <style>
     .registrygoback {
         background-color: #FFDF00;
@@ -19,5 +22,37 @@ function goBack() {
     <img src="/icons/xmas139.png" alt="Registry" />
     <h1>Registry Information<br>Coming Soon!</h1>
     <button class="btn btn-default btn-lg registrygoback" onclick="goBack()"><span class="glyphicon glyphicon-arrow-left"></span> Go Back</button>
+</div> -->
+
+<div class="registrylogocontain">
+    <h1>Our Registries</h1>
+    <div class="fullwithcap">
+        <div class="registrylogo">
+            <a href="https://www.walmart.com/lists/view-wedding-registry-items?id=3e32be8f-3210-4226-b431-ea29126f6e94" target="_blank">
+                <img src="/images/registrylogos/walmart.png" alt="Walmart Registry" style="padding-top: 100px;" />
+            </a>
+        </div>
+        <h2>Walmart</h2>
+        <p><a href="https://www.walmart.com/lists/view-wedding-registry-items?id=3e32be8f-3210-4226-b431-ea29126f6e94" target="_blank">View Registry</a></p>
+    </div>
+    <div class="fullwithcap">
+        <div class="registrylogo">
+            <a href="http://www.target.com/gift-registry/giftgiver?registryId=f8DH_Emlpr853-qVlovAcA&registryType=WEDDING" target="_blank">
+                <img src="/images/registrylogos/target.png" alt="Target Registry" style="padding-top: 45px;width: 115px;"/>
+                </a>
+        </div>
+        <h2>Target</h2>
+        <p><a href="http://www.target.com/gift-registry/giftgiver?registryId=f8DH_Emlpr853-qVlovAcA&registryType=WEDDING" target="_blank">View Registry</a></p>
+    </div>
+    <div class="fullwithcap">
+        <div class="registrylogo">
+            <a href="https://www.amazon.com/registry/wedding/1EQB9C357VRVV" target="_blank">
+                <img src="/images/registrylogos/amazon.png" alt="Amazon Registry" style="padding-top: 110px;" />
+            </a>
+        </div>
+        <h2>Amazon</h2>
+        <p><a href="https://www.amazon.com/registry/wedding/1EQB9C357VRVV" target="_blank">View Registry</a></p>
+    </div>
 </div>
+
 <?php include("footer.php"); ?>
