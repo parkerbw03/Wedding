7cbdb7d6ca49e746c49f0838eca13c3f171d49ca
Ben Parker <parkerbw03@gmail.com>
2016-02-20 16:53:19 -0600

    Added documentation

--- registry.php
+++ registry.php
@@ -1,28 +1,6 @@
 <?php include("nav.php"); ?>
 
-<!--
-<script>
-function goBack() {
-    window.history.back();
-}
-</script>
-
-<style>
-    .registrygoback {
-        background-color: #FFDF00;
-        color: #061934;
-    }
-    .registrygoback:hover {
-        background-color: #FFDF00;
-        color: #061934;
-        opacity: .8;
-    }
-</style>
-<div style="text-align:center;padding-bottom: 100px;padding-top: 100px;">
-    <img src="/icons/xmas139.png" alt="Registry" />
-    <h1>Registry Information<br>Coming Soon!</h1>
-    <button class="btn btn-default btn-lg registrygoback" onclick="goBack()"><span class="glyphicon glyphicon-arrow-left"></span> Go Back</button>
-</div> -->
+<!-- REGISTRY PAGE -->
 
 <div class="registrylogocontain">
     <h1>Our Registries</h1>
